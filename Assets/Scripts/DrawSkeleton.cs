dc849ac560093aa546b508376bb8689c8a5e7d4d
Justin Toribio <toribio.justin@gmail.com>
2021-07-08 18:53:24 -0700

    DrawSkeleton.cs added with initial variables

---
+++ Assets/Scripts/DrawSkeleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawSkeleton : MonoBehaviour
+{
+    [Tooltip("The list of key point GameObjects that make up the pose skeleton")]
+    public GameObject[] keypoints;
+
+    // The GameObjects that contain data for the lines between key points
+    private GameObject[] lines;
+
+    // The line renderers that draw the lines between key points
+    private LineRenderer[] lineRenderers;
+
+    // The pairs of key points that should be connected on a body
+    private int[][] jointPairs;
+
+    // The width for the skeleton lines
+    private float lineWidth = 5.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
